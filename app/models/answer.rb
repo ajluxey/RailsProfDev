58ad15fb3bfb505b4de327a6f62871d7bab611a2
ajluxey <v_koralina@list.ru>
2021-08-18 20:13:39 +0500

    Rework test for validation and rework validation in model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  validates :body, :correct, presence: true
+  # validates :body, :correct, presence: true
+
+  validates :body,    presence: true
+  validates :correct, inclusion: { in: [true, false] }
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,5 +2,5 @@ RSpec.describe Answer, type: :model do
   it { should belong_to :question }
 
   it { should validate_presence_of :body }
-  it { should validate_presence_of :correct }
+  it { should validate_inclusion_of(:correct).in_array([true, false]) }
 end
